3b3fe7db8009762589baa3b5bcb934acaa7c2b39
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-07-27 14:27:28 +0200

    check to see if I can remove some dependancies

--- functions/save.js
+++ functions/save.js
@@ -1,81 +1,70 @@
 const AWS = require("aws-sdk");
 require("dotenv").load();
 require("isomorphic-fetch");
-const path = require("path");
-const fs = require("fs-extra");
+//const path = require("path");
+//const fs = require("fs-extra");
 const Dropbox = require("dropbox").Dropbox;
 
 // Configure client for use with Spaces
 const spacesEndpoint = new AWS.Endpoint(process.env.S3_ENDPOINT);
 const s3 = new AWS.S3({
-  endpoint: spacesEndpoint,
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET,
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.S3_KEY,
+    secretAccessKey: process.env.S3_SECRET,
 });
 
 // Setup our interface for the Dropbox API with our token
 // If there's no token, we'll just go ahead and exit this script now.
 if (!process.env.DBX_ACCESS_TOKEN) {
-  console.log(
-    "Error: could not find a Dropbox access token. Make sure you have a `.env` file with a `DBX_ACCESS_TOKEN` key/value pair for accessing the Dropbox API."
-  );
-  process.exit(1);
+    console.log(
+        "Error: could not find a Dropbox access token. Make sure you have a `.env` file with a `DBX_ACCESS_TOKEN` key/value pair for accessing the Dropbox API."
+    );
+    process.exit(1);
 }
 const dbx = new Dropbox({
-  accessToken: process.env.DBX_ACCESS_TOKEN,
+    accessToken: process.env.DBX_ACCESS_TOKEN,
 });
 
-
 exports.handler = function(event, context, callback) {
+    // Get all the posts in the root of our our Dropbox App's directory and save
+    // them all to our local posts folder.
+    dbx
+        .filesListFolder({
+            path: "",
+        })
+        .then((response) => {
+            response.entries.forEach((entry) => {
+                const { name, path_lower } = entry;
 
+                if (entry[".tag"] === "file") {
+                    dbx
+                        .filesDownload({
+                            path: path_lower,
+                        })
+                        .then((data) => {
+                            const filecontents = data.fileBinary.toString();
 
-  // Get all the posts in the root of our our Dropbox App's directory and save
-  // them all to our local posts folder.
-  dbx
-    .filesListFolder({
-      path: ""
-    })
-    .then((response) => {
-      response.entries.forEach((entry) => {
-        const {
-          name,
-          path_lower
-        } = entry;
+                            // save the file
+                            var params = {
+                                Body: filecontents,
+                                Bucket: process.env.S3_BUCKET,
+                                Key: name,
+                                ContentType: "text/plain",
+                                ACL: "public-read",
+                            };
 
-        if (entry[".tag"] === "file") {
-          dbx
-            .filesDownload({
-              path: path_lower
-            })
-            .then((data) => {
-
-              const filecontents = data.fileBinary.toString();
-
-              // save the file
-              var params = {
-                Body: filecontents,
-                Bucket: process.env.S3_BUCKET,
-                Key: name,
-                ContentType: "text/plain",
-                ACL: "public-read",
-              };
-
-              s3.putObject(params, function(err, data) {
-                if (err) console.log(err, err.stack);
-                else console.log(data);
-              });
-
-            })
-            .catch((error) => {
-              console.log("Error: file failed to download", name, error);
+                            s3.putObject(params, function(err, data) {
+                                if (err) console.log(err, err.stack);
+                                else console.log(data);
+                            });
+                        })
+                        .catch((error) => {
+                            console.log("Error: file failed to download", name, error);
+                        });
+                }
             });
-        }
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-
-
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 };
